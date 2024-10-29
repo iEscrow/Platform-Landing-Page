6c549d1bca8b61a0fae5adbf151826f6411bd852
TomasSorgetti <tomassorgetti456@gmail.com>
2024-10-29 03:13:30 -0300

    feat: add translations setup with English and Spanish languages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,17 @@
+import { useTranslation } from 'react-i18next';
+
 export default function Home() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
   return (
     <>
-      <h1>Home Page</h1>
+      <h1>{t('Home.title')}</h1>
+      <p>{t('Home.hello')}</p>
+      <button onClick={() => changeLanguage('en')}>English</button>
+      <button onClick={() => changeLanguage('es')}>Spanish</button>
     </>
   );
 }
