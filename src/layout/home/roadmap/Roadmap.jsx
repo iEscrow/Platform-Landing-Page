4e389d92332569152be50880b8c90fa45fa836e6
TomasSorgetti <tomassorgetti456@gmail.com>
2025-01-08 18:36:52 -0300

    feat: roadmap translations

--- src/layout/home/roadmap/Roadmap.jsx
+++ src/layout/home/roadmap/Roadmap.jsx
@@ -1,5 +1,6 @@
 import styles from './Roadmap.module.css';
 import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import GradientTitle from '@components/gradientTitle/GradientTitle';
 import CountdownTimer from '@features/countdown/components/CountdownTimer';
@@ -12,15 +13,45 @@ import mobileLine1 from '@assets/images/roadmap/line_1_mobile.svg';
 import mobileLine2 from '@assets/images/roadmap/line_2_mobile.svg';
 
 export default function Roadmap() {
+  const { t } = useTranslation();
   const { theme } = useContext(ThemeContext);
+
   const tokenPresaleDate = new Date('2025-01-01T00:00:00Z').getTime();
   const tokenPresaleCountdown = useCountdown(tokenPresaleDate);
-
   const tokenLaunchDate = new Date('2025-01-09T00:00:00Z').getTime();
   const tokenLaunchCountdown = useCountdown(tokenLaunchDate);
-
   const exchangeLaunchDate = new Date('2025-01-20T00:00:00Z').getTime();
   const exchangeLaunchCountdown = useCountdown(exchangeLaunchDate);
+
+  const roadmapEvents = [
+    {
+      id: 'exchangeLaunch',
+      title: t('Home.Roadmap.exchangeLaunch.title'),
+      currency: t('Home.Roadmap.exchangeLaunch.currency'),
+      description: t('Home.Roadmap.exchangeLaunch.description'),
+      date: t('Home.Roadmap.exchangeLaunch.date'),
+      countdown: exchangeLaunchCountdown,
+      isCountdownVisible: tokenPresaleCountdown !== null,
+    },
+    {
+      id: 'tokenLaunch',
+      title: t('Home.Roadmap.tokenLaunch.title'),
+      currency: t('Home.Roadmap.tokenLaunch.currency'),
+      description: t('Home.Roadmap.tokenLaunch.description'),
+      date: t('Home.Roadmap.tokenLaunch.date'),
+      countdown: tokenLaunchCountdown,
+      isCountdownVisible: tokenPresaleCountdown === null,
+    },
+    {
+      id: 'tokenPresale',
+      title: t('Home.Roadmap.tokenPresale.title'),
+      currency: t('Home.Roadmap.tokenPresale.currency'),
+      description: t('Home.Roadmap.tokenPresale.description'),
+      date: t('Home.Roadmap.tokenPresale.date'),
+      countdown: tokenPresaleCountdown,
+      isCountdownVisible: tokenPresaleCountdown !== null,
+    },
+  ];
 
   return (
     <section className={`${styles.roadmap} ${styles[theme]}`}>
@@ -33,41 +64,35 @@ export default function Roadmap() {
         <img src={timeLine} alt="roadmap time line" />
       </div>
       <ul className={styles.list}>
-        <li className={styles.exchangeLaunch}>
-          <div className={styles.container}>
-            <h3>Exchange Launch</h3>
-            <p>Cryptocurrency</p>
-            <p>iESCROW exchange launch</p>
-            <span className={styles.date}>1st September 2025</span>
-            {tokenLaunchCountdown === null && (
-              <CountdownTimer timeLeft={exchangeLaunchCountdown} variant="sm" />
-            )}
-          </div>
-          <img src={mobileLine1} alt="decoration line" />
-        </li>
-
-        <li className={styles.tokenLaunch}>
-          <div className={styles.container}>
-            <h3>Token Launch</h3>
-            <p>Cryptocurrency</p>
-            <p>$ESCROW token launch</p>
-            <span className={styles.date}>15th August 2025</span>
-            {tokenPresaleCountdown === null && (
-              <CountdownTimer timeLeft={tokenLaunchCountdown} variant="sm" />
-            )}
-          </div>
-          <img src={mobileLine2} alt="decoration line" />
-        </li>
-
-        <li className={styles.tokenPresale}>
-          <div className={styles.container}>
-            <h3>Token Presale</h3>
-            <p>Cryptocurrency</p>
-            <p>$ESCROW presale starts</p>
-            <span className={styles.date}>1st July 2025</span>
-            <CountdownTimer timeLeft={tokenPresaleCountdown} variant="sm" />
-          </div>
-        </li>
+        {roadmapEvents.map(
+          ({
+            id,
+            title,
+            currency,
+            description,
+            date,
+            countdown,
+            isCountdownVisible,
+          }) => (
+            <li key={id} className={styles[id]}>
+              <div className={styles.container}>
+                <h3>{title}</h3>
+                <p>{currency}</p>
+                <p>{description}</p>
+                <span className={styles.date}>{date}</span>
+                {isCountdownVisible && (
+                  <CountdownTimer timeLeft={countdown} variant="sm" />
+                )}
+              </div>
+              {id !== 'tokenPresale' && (
+                <img
+                  src={id === 'exchangeLaunch' ? mobileLine1 : mobileLine2}
+                  alt="decoration line"
+                />
+              )}
+            </li>
+          )
+        )}
       </ul>
     </section>
   );
