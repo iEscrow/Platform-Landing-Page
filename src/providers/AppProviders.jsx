863d8f462202a799ac1f9d6776865c7b7325fdf2
TomasSorgetti <tomassorgetti456@gmail.com>
2025-02-05 11:55:08 -0300

    fix: optimize App providers component & documented

--- src/providers/AppProviders.jsx
+++ src/providers/AppProviders.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { ThemeProvider } from '../context/ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { CurrencyProvider } from '../context/CurrencyContext';
 import { HelmetProvider } from 'react-helmet-async';
 
+/**
+ * AppProviders is a wrapper component that provides the necessary contexts to the app.
+ * It wraps the application with:
+ * - BrowserRouter for routing
+ * - CurrencyProvider for currency context
+ * - HelmetProvider for managing document head metadata
+ * - ThemeProvider for theme-related context
+ *
+ * @param {React.PropsWithChildren} props - The children to be wrapped by the providers.
+ * @returns {JSX.Element} - The wrapped application component.
+ */
 const AppProviders = ({ children }) => {
   return (
     <BrowserRouter>
@@ -17,8 +28,9 @@ const AppProviders = ({ children }) => {
   );
 };
 
+// Memoize the component to prevent unnecessary re-renders
+export default memo(AppProviders);
+
 AppProviders.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
-export default AppProviders;
