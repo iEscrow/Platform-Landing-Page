9558e3fc24c7f173d67ce1cc0cf66bfb7b510e7a
TomasSorgetti <tomassorgetti456@gmail.com>
2025-02-05 11:18:43 -0300

    refactor: useClickOutside hook updated & documented

--- src/hooks/useClickOutside.jsx
+++ src/hooks/useClickOutside.jsx
@@ -1,6 +1,27 @@
 import { useEffect } from 'react';
 
-export default function useClickOutside(ref, callback) {
+/**
+ * Custom hook to detect clicks outside of a specified element and trigger a callback.
+ *
+ * @param {React.RefObject} ref - A React ref pointing to the element you want to detect outside clicks for.
+ * @param {Function} callback - The function to be called when an outside click is detected.
+ * @param {Array<string>} [eventTypes=['mousedown', 'touchstart']] - Optional array of event types to listen for.
+ *
+ * @example
+ * const ref = useRef(null);
+ * useClickOutside(ref, () => setIsOpen(false));
+ *
+ * return (
+ *   <div ref={ref}>
+ *     Dropdown Content
+ *   </div>
+ * );
+ */
+export default function useClickOutside(
+  ref,
+  callback,
+  eventTypes = ['mousedown', 'touchstart']
+) {
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (ref.current && !ref.current.contains(event.target)) {
@@ -8,9 +29,14 @@ export default function useClickOutside(ref, callback) {
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    eventTypes.forEach((eventType) => {
+      document.addEventListener(eventType, handleClickOutside);
+    });
+
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      eventTypes.forEach((eventType) => {
+        document.removeEventListener(eventType, handleClickOutside);
+      });
     };
-  }, [ref, callback]);
+  }, [ref, callback, eventTypes]);
 }
